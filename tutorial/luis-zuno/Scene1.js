d03e72c51675ed5d4f8384d529c431cd2af12ed4
Ray Chung <rchung725@gmail.com>
2020-08-04 14:28:43 -0400

    part 3

--- tutorial/luis-zuno/Scene1.js
+++ tutorial/luis-zuno/Scene1.js
@@ -3,8 +3,15 @@ class Scene1 extends Phaser.Scene {
 	super("bootGame");
     }
 
+    preload() {
+        this.load.image("background","assets/images/background.png");
+        this.load.image("ship", "assets/images/ship.png");
+        this.load.image("ship1", "assets/images/ship2.png");
+        this.load.image("ship2", "assets/images/ship3.png");
+    }
+
     create() {
-	this.add.text(20, 20, "Loading game...");
-	this.scene.start("playGame");
+	    this.add.text(20, 20, "Loading game...");
+	    this.scene.start("playGame");
     }
 }
